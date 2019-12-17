3b23354f5f5cfc9bf6b88a41fa0a47b3568b896a
Cooltimmetje <Cooltimmetje@me.com>
2019-12-17 13:40:22 +0100

    remove spacing form UserStat.java

--- src/main/java/me/Cooltimmetje/Skuddbot/Enums/UserStat.java
+++ src/main/java/me/Cooltimmetje/Skuddbot/Enums/UserStat.java
@@ -12,26 +12,26 @@ import lombok.Getter;
 @Getter
 public enum UserStat {
 
-    XP                          ("xp",                      ValueType.INTEGER, "Experience",             "xp",        "0", true,  StatCategory.NO_CATEGORY     ),
-    CHALLENGE_WINS              ("chlng_wins",              ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.CHALLENGE       ),
-    CHALLENGE_LOSSES            ("chlng_losses",            ValueType.INTEGER, "Losses",                 "Losses",    "0", true,  StatCategory.CHALLENGE       ),
-    CHALLENGE_WINSTREAK         ("chlng_winstreak",         ValueType.INTEGER, "Winstreak",              "wins",      "0", true,  StatCategory.CHALLENGE       ),
-    CHALLENGE_LONGEST_WINSTREAK ("chlng_longest_winstreak", ValueType.INTEGER, "Longest winstreak",      "wins",      "0", true,  StatCategory.CHALLENGE       ),
-    FFA_WINS                    ("ffa_wins",                ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.FREE_FOR_ALL    ),
-    FFA_LOSSES                  ("ffa_losses",              ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.FREE_FOR_ALL    ),
-    FFA_HIGHEST_WIN             ("ffa_highest_win",         ValueType.INTEGER, "Most entrants win",      "entrants",  "0", true,  StatCategory.FREE_FOR_ALL    ),
-    FFA_KILLS                   ("ffa_kills",               ValueType.INTEGER, "Kills",                  "kills",     "0", true,  StatCategory.FREE_FOR_ALL    ),
-    BJ_WINS                     ("bj_wins",                 ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.BLACKJACK       ),
-    BJ_LOSSES                   ("bj_losses",               ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.BLACKJACK       ),
-    BJ_PUSHES                   ("bj_pushes",               ValueType.INTEGER, "Pushes",                 "pushes",    "0", true,  StatCategory.BLACKJACK       ),
-    BJ_TWENTY_ONES              ("bj_twenty_ones",          ValueType.INTEGER, "21's",                   "21's",      "0", true,  StatCategory.BLACKJACK       ),
-    TD_WINS                     ("td_wins",                 ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_LOSSES                   ("td_losses",               ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_DEFENSES                 ("td_defences",             ValueType.INTEGER, "Teammate defences",      "defences",  "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_HIGHEST_WIN              ("td_highest_win",          ValueType.INTEGER, "Most entrants win",      "entrants",  "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_ALL_SURVIVED             ("td_all_survived",         ValueType.INTEGER, "All teammates survived", "times",     "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_KILLS                    ("td_kills",                ValueType.INTEGER, "Kills",                  "kills",     "0", true,  StatCategory.TEAM_DEATHMATCH ),
-    TD_TEAMMATES                ("td_teammates",            ValueType.STRING,  "Kills",                  "kills",     "0", false, StatCategory.TEAM_DEATHMATCH );
+    XP                          ("xp",                      ValueType.INTEGER, "Experience",             "xp",        "0", true,  StatCategory.NO_CATEGORY    ),
+    CHALLENGE_WINS              ("chlng_wins",              ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.CHALLENGE      ),
+    CHALLENGE_LOSSES            ("chlng_losses",            ValueType.INTEGER, "Losses",                 "Losses",    "0", true,  StatCategory.CHALLENGE      ),
+    CHALLENGE_WINSTREAK         ("chlng_winstreak",         ValueType.INTEGER, "Winstreak",              "wins",      "0", true,  StatCategory.CHALLENGE      ),
+    CHALLENGE_LONGEST_WINSTREAK ("chlng_longest_winstreak", ValueType.INTEGER, "Longest winstreak",      "wins",      "0", true,  StatCategory.CHALLENGE      ),
+    FFA_WINS                    ("ffa_wins",                ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.FREE_FOR_ALL   ),
+    FFA_LOSSES                  ("ffa_losses",              ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.FREE_FOR_ALL   ),
+    FFA_HIGHEST_WIN             ("ffa_highest_win",         ValueType.INTEGER, "Most entrants win",      "entrants",  "0", true,  StatCategory.FREE_FOR_ALL   ),
+    FFA_KILLS                   ("ffa_kills",               ValueType.INTEGER, "Kills",                  "kills",     "0", true,  StatCategory.FREE_FOR_ALL   ),
+    BJ_WINS                     ("bj_wins",                 ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.BLACKJACK      ),
+    BJ_LOSSES                   ("bj_losses",               ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.BLACKJACK      ),
+    BJ_PUSHES                   ("bj_pushes",               ValueType.INTEGER, "Pushes",                 "pushes",    "0", true,  StatCategory.BLACKJACK      ),
+    BJ_TWENTY_ONES              ("bj_twenty_ones",          ValueType.INTEGER, "21's",                   "21's",      "0", true,  StatCategory.BLACKJACK      ),
+    TD_WINS                     ("td_wins",                 ValueType.INTEGER, "Wins",                   "wins",      "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_LOSSES                   ("td_losses",               ValueType.INTEGER, "Losses",                 "losses",    "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_DEFENSES                 ("td_defences",             ValueType.INTEGER, "Teammate defences",      "defences",  "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_HIGHEST_WIN              ("td_highest_win",          ValueType.INTEGER, "Most entrants win",      "entrants",  "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_ALL_SURVIVED             ("td_all_survived",         ValueType.INTEGER, "All teammates survived", "times",     "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_KILLS                    ("td_kills",                ValueType.INTEGER, "Kills",                  "kills",     "0", true,  StatCategory.TEAM_DEATHMATCH),
+    TD_TEAMMATES                ("td_teammates",            ValueType.STRING,  "Kills",                  "kills",     "0", false, StatCategory.TEAM_DEATHMATCH);
 
     private String dbReference;
     private ValueType type;
