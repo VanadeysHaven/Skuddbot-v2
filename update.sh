4105461056b834a5b6c38cd4dbf0e7884b09adca
VanadeysHaven <Cooltimmetje@me.com>
2022-05-30 23:21:00 +0200

    database backup complete with skip

--- update.sh
+++ update.sh
@@ -1,23 +1,35 @@
+echo "Setting up..."
+mkdir database/import_data -p
+mkdir backups -p
+
 echo "Pulling latest version from Git..."
 git pull
 
 commit=$(git log --pretty=format:'%h' -n 1)
 branch=$(git branch --show-current)
 time_file=$(date +%Y%m%d%H%M%S)
-time_bot=$(date "+%Y-%m-%d %H:%M:%S \(UTC\)")
+time_bot=$(date "+%d-%m-%Y %H:%M:%S \(UTC\)")
 
 echo "Deploying $branch > $commit"
 echo "Updating configs"
 rm configs/git.env -f
 touch configs/git.env
-echo "COMMIT=$commit" >> configs/git.env
-echo "BRANCH=$branch" >> configs/git.env
-echo "DEPLOY_TIME=$time_bot" >> configs/git.env
+{
+  echo "COMMIT=$commit
+  BRANCH=$branch
+  DEPLOY_TIME=$time_bot"
+} >> configs/git.env
 
-if [ ! "$(docker ps -a | grep skuddbot_v2_database)" ];
+database_state=$(sudo docker inspect --format="{{.State.Running}}" skuddbot_v2_database)
+if [[ $*  == "--skip-db" ]];
 then
-  echo "Backing up database..."
-  sudo docker exec -it skuddbot_v2_database sh -c 'exec mysqldump ${MYSQL_DATABASE} -uroot -p"${MYSQL_ROOT_PASSWORD}"' > backups/backup-$time_file.sql
+  echo "Skipping backup database"
+else
+  if [ "$database_state" == "true" ];
+  then
+    echo "Backing up database..."
+    sudo docker exec -it skuddbot_v2_database sh -c 'exec mysqldump ${MYSQL_DATABASE} -uroot -p"${MYSQL_ROOT_PASSWORD}"' | sudo tee backups/backup-"$time_file".sql > /dev/null
+  fi
 fi
 
 echo "Shutting down bot..."
